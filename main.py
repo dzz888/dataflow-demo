64c661fdf9624d65f547afb140ba4a802a4f3675
dzz888 <59912794+dzz888@users.noreply.github.com>
2023-04-24 08:46:01 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import apache_beam as beam
+
+
+def run():
+    with beam.Pipeline() as pipeline:
+        print('hello')
+        # Add your pipeline logic here
+        pass
+
+if __name__ == '__main__':
+  run()        
